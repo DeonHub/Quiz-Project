77a6476f547b49624a5cb528b13271eb6a926593
Gideon Impraim <82456010+GI-10836535@users.noreply.github.com>
2021-08-19 15:44:29 +0000

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,56 @@
+var questions = [
+{
+"question" : "C is a.............",
+"option1" : "A third generation high level language",
+"option2" : "A machine language",
+"option3" : "An assembly language",
+"option4" : "All of the above",
+"answer" : "1",
+},
+
+{
+"question" : "Web pages are written using?",
+"option1" : "FTP",
+"option2" : "HTTP",
+"option3" : "HTML",
+"option4" : "URL",
+"answer" : "3",
+},
+
+{
+"question" : "Which of the following is an input device?",
+"option1" : "Speaker",
+"option2" : "Monitor",
+"option3" : "Keyboard",
+"option4" : "Cable",
+"answer" : "3",
+},
+
+{
+"question" : "What is the first stage of the Information Processing Cycle?",
+"option1" : "Storage stage",
+"option2" : "Output stage",
+"option3" : "Processing stage",
+"option4" : "Input stage",
+"answer" : "4",
+},
+
+{
+"question" : "What is the full meaning of RAM?",
+"option1" : "Read About Me",
+"option2" : "Random Access Memory",
+"option3" : "Random Access Main",
+"option4" : "Read Access Memory",
+"answer" : "2",
+},
+
+{
+"question" : "An intersection of a row and a column is known as......",
+"option1" : "Cell",
+"option2" : "Rows",
+"option3" : "Worksheet",
+"option4" : "Data",
+"answer" : "1",
+}
+
+];
